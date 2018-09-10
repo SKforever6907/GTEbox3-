860d29493ea9b4fab01f6f787ab0830aef20ac55
your nameskforever git config --global user.email 553939139@qq.comgit remote add origin git@github.com:skforeve/yourRepo.gitgit config --global user.name skforever6907git config --global user.name your <553939139@qq.com>
2018-09-10 20:43:25 +0800

    V2.0

---
+++ I2C.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <errno.h>
+#define I2C_ADDR 0x23
+int main(void)
+{
+    int fd;
+	int write_fd;
+    char buf[3];
+    char val,value;
+	char buffer[4];
+    float flight;
+    fd=open("/dev/i2c-1",O_RDWR);
+    if(fd<0)
+    {
+        printf("打开文件错误:%s\r\n",strerror(errno)); return 1;
+    }
+    if(ioctl( fd,I2C_SLAVE,I2C_ADDR)<0 )
+    {
+        printf("ioctl 错误 : %s\r\n",strerror(errno));return 1;
+    }
+    val=0x01;
+    if(write(fd,&val,1)<0)
+    {
+        printf("上电失败\r\n");
+    }
+    val=0x11;
+    if(write(fd,&val,1)<0)
+    {
+        printf("开启高分辨率模式2\r\n");
+    }
+    usleep(200000);
+    if(read(fd,&buf,3)){
+        flight=(buf[0]*256+buf[1])*0.5/1.2;
+        printf("光照度: %6.2flx\r\n",flight);
+		write_fd=open("./fifo",O_WRONLY);
+		sprintf(buffer,"%.3f",flight);
+		ret=write(write_fd,buffer,10);
+		close(write_fd);
+    }
+    else{
+        printf("读取错误\r\n");
+    }
+}
